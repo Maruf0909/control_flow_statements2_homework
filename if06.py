268d706671ed09b55f0a3eb0c9f3719d00e4a3d7
Mac <mac@MacBook-Air-Mac.local>
2023-07-03 01:16:00 +0500

    if06

--- if06.py
+++ if06.py
@@ -6,4 +6,32 @@ def main(n):
     Returns:
         int: return answer.
     """
-    return
+    x1=n%10
+    n=n//10
+    x2=n%10
+    n=n//10
+    x3=n%10
+    n=n//10
+    x4=n%10
+    n=n//10
+    x5=n%10
+    n=n//10
+
+    max=x1
+    idx=1
+    if max<x2:
+        max=x2
+        idx=2
+    if max<x3:
+        max=x3
+        idx=3
+    if max<x4:
+        max=x4
+        idx=4
+    if max<x5:
+        max=x5
+        idx=5
+
+    return idx
+
+print(main(76514))
